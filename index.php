b725ca10bde44e476fe747fa1e034c2fbfa25336
Elvis Souza <elvis.pereira.souza@gmail.com>
2022-05-19 13:59:17 -0300

    teste finalizado

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+   echo "Olá mundo";
+
+   echo "Hello World";
+
+   echo "Meu primeiro código PHP";
+
+   echo "kate amo voce";
+
+   echo "KATE SOUZA";
+
+   echo "ELVIS SOUZA";
+
+?>   
